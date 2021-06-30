5a5da030f46a524014d34daf93973e97041294fe
chandanadsc15 <86706545+chandanadsc15@users.noreply.github.com>
2021-06-30 11:51:44 +0530

    Add files via upload

---
+++ spsite_new.ps1
@@ -0,0 +1,51 @@
+﻿Configuration SharePoint
+{
+    param(
+        [Parameter(Mandatory = $true)]
+        [PSCredential]
+        $credsGlobalAdmin
+    )
+    Import-DscResource -ModuleName Microsoft365DSC
+
+    node localhost
+    {
+        SPOSite DemoSite
+        {
+            Url                                         = "https://dsc15.sharepoint.com/sites/test3"
+            StorageMaximumLevel                         = 26214400
+            LocaleId                                    = 1033
+            Template                                    = "STS#3"
+            GlobalAdminAccount                          = $credsGlobalAdmin
+            Owner                                       = "csasi@dsc15.onmicrosoft.com"
+            Title                                       = "TestSite"
+            TimeZoneId                                  = 13
+            Ensure                                      = "Present"
+            StorageWarningLevel                         = 25574400
+            SharingCapability                           = "Disabled"
+            CommentsOnSitePagesDisabled                 = $false
+            DisableAppViews                             = "NotDisabled"
+            DisableCompanyWideSharingLinks              = "NotDisabled"
+            DisableFlows                                = $false
+            DefaultSharingLinkType                      = "None"
+            DefaultLinkPermission                       = "None"
+        }
+    }
+}
+$cd = @{
+    AllNodes = @(
+        @{
+            NodeName = 'localhost'
+            PSDscAllowDomainUser = $true
+            PSDscAllowPlainTextPassword = $true
+        }
+    )
+
+NonNodeData = @(
+        @{
+            # API Token for the Teams SDN Provider for Meeting Broadcast
+            SdnApiToken = "**********"
+
+        }
+    )
+}
+SharePoint  -ConfigurationData $cd
---
+++ teams.ps1
@@ -0,0 +1,63 @@
+﻿Configuration CreatingTeam
+{param(
+        [Parameter(Mandatory = $true)]
+        [PSCredential]
+        $credsGlobalAdmin
+    )
+
+    Import-DscResource -ModuleName Microsoft365DSC
+         
+
+    node localhost
+    {
+        TeamsTeam MyTeam2
+        {
+            DisplayName                       = "Offshore"
+            Description                       = "Offshore"
+            Visibility                        = "Private"
+            MailNickName                      = "Offshore"
+            Owner="csasi@dsc15.onmicrosoft.com"
+
+            AllowUserEditMessages             = $false
+            AllowUserDeleteMessages           = $false
+            AllowOwnerDeleteMessages          = $false
+            AllowTeamMentions                 = $false
+            AllowChannelMentions              = $false
+            allowCreateUpdateChannels         = $false
+            AllowDeleteChannels               = $false
+            AllowAddRemoveApps                = $false
+            AllowCreateUpdateRemoveTabs       = $false
+            AllowCreateUpdateRemoveConnectors = $false
+            AllowGiphy                        = $True
+            GiphyContentRating                = "strict"
+            AllowStickersAndMemes             = $True
+            AllowCustomMemes                  = $True
+            AllowGuestCreateUpdateChannels    = $true
+            AllowGuestDeleteChannels          = $true
+           GlobalAdminAccount                = $credsGlobalAdmin
+            Ensure                            = "Present"
+        }
+        TeamsChannel MyChannel
+        {
+            TeamName           = "Offshore"
+            DisplayName        = "Cofco1"
+            NewDisplayName     = "Cofco1"
+            Description        = "Testing"
+            Ensure             = "Present"
+           
+           GlobalAdminAccount = $credsGlobalAdmin
+        }
+    }
+}
+$cd = @{
+    AllNodes = @(
+        @{
+            NodeName = 'localhost'
+            PSDscAllowDomainUser = $true
+            PSDscAllowPlainTextPassword = $true
+        }
+    )
+}
+CreatingTeam -ConfigurationData $cd
+Start-DscConfiguration CreatingTeam -force -Verbose -Wait
+
